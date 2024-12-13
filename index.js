131ef3a7c732527489d816b60ecebfe8a9e33824
Mustm4 <145650653+Mustm4@users.noreply.github.com>
2024-12-13 11:49:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 document.querySelector('.ticket-form').addEventListener('submit', async (event) => {
-    console.log("Form submitted");  // Lägg till detta för att se om eventet tas emot.
+    console.log("Form submitted");
     event.preventDefault();
 
     const orderId = document.getElementById('orderid').value;
     const resultDiv = document.getElementById('qr-code-result');
+    const instructions = document.querySelector('.instructions');
+    const form = document.querySelector('.ticket-form');
+
+    // Döljer instruktioner och formulär
+    instructions.style.display = 'none';
+    form.style.display = 'none';
 
     // Clear previous results
     resultDiv.innerHTML = '';
@@ -12,9 +18,9 @@ document.querySelector('.ticket-form').addEventListener('submit', async (event) 
         const response = await fetch(`https://stripewebhook-function.azurewebsites.net/api/GetQRCodeImage?orderId=${orderId}`, {
             cache: 'no-store', // För att undvika cache-problem
         });
-        
+
         console.log('Response:', response);  // Logga hela svaret
-        
+
         if (response.ok) {
             const data = await response.json();
             console.log('Fetched data:', data);  // Logga JSON-responsen
@@ -28,7 +34,7 @@ document.querySelector('.ticket-form').addEventListener('submit', async (event) 
             console.error('Error response:', response.status, response.statusText);
             resultDiv.textContent = `Error fetching QR code: ${response.statusText}`;
         }
-        
+
     } catch (error) {
         console.error('Fetch error:', error);
         resultDiv.textContent = 'An error occurred while retrieving your QR code.';
